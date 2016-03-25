048e140e34ccbd9b6aa84c7019bde171bcfa6e63
Martin Ahrnbom <Martin Ahrnbom>
2016-03-25 12:10:27 +0100

    Fixed picky compiler issue

--- luasocket-uwp/libluasocket/luasocket.c
+++ luasocket-uwp/libluasocket/luasocket.c
@@ -112,7 +112,7 @@ static int base_open(lua_State *L) {
 /*-------------------------------------------------------------------------*\
 * Initializes all library modules.
 \*-------------------------------------------------------------------------*/
-int LUASOCKET_API luaopen_socket_core(lua_State *L) {
+LUASOCKET_API int luaopen_socket_core(lua_State *L) {
     int i;
     base_open(L);
     for (i = 0; mod[i].name; i++) mod[i].func(L);
